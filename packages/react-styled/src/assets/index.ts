b0bc5dda96946b02f244dcffcf9f9fb7467587ef
alikhere <alikhurshid.dev@gmail.com>
2026-03-29 05:05:30 +0530

    feat(react-styled): bundle racha-fire PNG assets and add blueimp-md5 dependency

--- packages/react-styled/src/assets/index.ts
+++ packages/react-styled/src/assets/index.ts
@@ -1,10 +1,16 @@
 import logoUrl from './storacha-logo.svg'
 import fireBgUrl from './racha-fire.jpg'
+import fireBgPngUrl from './racha-fire.png'
+import fireBgHalfUrl from './racha-fire-opacity-50.png'
 
 export const storachaLogoUrl = logoUrl
 export const rachaFireBgUrl = fireBgUrl
+export const rachaFireBgPngUrl = fireBgPngUrl
+export const rachaFireBgHalfUrl = fireBgHalfUrl
 
 export default {
   logo: logoUrl,
   fireBg: fireBgUrl,
+  fireBgPng: fireBgPngUrl,
+  fireBgHalf: fireBgHalfUrl,
 }
